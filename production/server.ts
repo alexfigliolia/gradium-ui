00086808bece59c5a7ff027201151762829255c3
Alex Figliolia <alexfigliolia@gmail.com>
2024-09-27 13:44:09 -0400

    Migrate back to GCP

--- production/server.ts
+++ production/server.ts
@@ -1,42 +1,51 @@
 import { default as Express, static as staticFiles } from "express";
 import { existsSync } from "fs";
+import helmet from "helmet";
 import { join } from "path";
 
-const app = Express();
-app.set("trust proxy", true);
+export class App {
+  public static instance = Express();
+  public static PORT = process.env.PORT || 8080;
+  public static buildDirectory = this.locateBuild();
 
-const buildPath: string[] = [process.cwd()];
-let N = 10;
-while (N > 0) {
-  N--;
-  console.log("testing", join(...buildPath));
-  if (
-    existsSync(join(...buildPath, "build")) &&
-    existsSync(join(...buildPath, "src"))
-  ) {
-    break;
-  } else {
-    buildPath.push("..");
+  public static initialize() {
+    this.registerMiddleware();
+    this.instance.listen(this.PORT, () => {
+      console.log("Server Running!");
+    });
+  }
+
+  private static registerMiddleware() {
+    this.instance.set("trust proxy", true);
+    this.instance.use(helmet());
+    this.instance.use((req, res, next) => {
+      if (/manifest.webmanifest/.test(req.path)) {
+        res.setHeader("Content-Type", "application/manifest+json");
+      }
+      next();
+    });
+    this.instance.use(
+      staticFiles(this.buildDirectory, { maxAge: 31557600, immutable: true }),
+    );
+  }
+
+  private static locateBuild() {
+    const buildPath: string[] = [process.cwd()];
+    let N = 10;
+    while (N > 0) {
+      N--;
+      console.log("testing", join(...buildPath));
+      if (
+        existsSync(join(...buildPath, "build")) &&
+        existsSync(join(...buildPath, "src"))
+      ) {
+        break;
+      } else {
+        buildPath.push("..");
+      }
+    }
+    return join(...buildPath, "build");
   }
 }
 
-const buildDirectory = join(...buildPath, "build");
-
-app.use((req, res, next) => {
-  if (
-    /(.ico|.js|.css|.jpg|.png|.map|.svg|.avif|.webp|.jpeg)$/i.test(req.path)
-  ) {
-    next();
-  } else {
-    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-    res.header("Expires", "-1");
-    res.header("Pragma", "no-cache");
-    res.sendFile(join(buildDirectory, "index.html"));
-  }
-});
-
-app.use(staticFiles(buildDirectory));
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log("Server Running!");
-});
+App.initialize();
