0cd07011e9db1853388cb726fa8fd5d01d83e92f
Alex Figliolia <alexfigliolia@gmail.com>
2025-01-01 13:50:18 -0500

    Some visual touchups

--- src/Components/Confirmation/index.tsx
+++ src/Components/Confirmation/index.tsx
@@ -1,5 +1,9 @@
-import { memo } from "react";
-import type { IBottomSheetProps } from "@figliolia/bottom-sheet";
+import type { ForwardedRef } from "react";
+import { forwardRef, memo } from "react";
+import type {
+  IBottomSheetProps,
+  ISheetController,
+} from "@figliolia/bottom-sheet";
 import { BottomSheet } from "@figliolia/bottom-sheet";
 import { useClassNames } from "@figliolia/classnames";
 import { BodyPortal } from "Components/BodyPortal";
@@ -8,21 +12,22 @@ import { selectWidth, useScreen } from "State/Screen";
 import { Devices } from "Tools/Devices";
 import "./styles.scss";
 
-export const Confirmation = memo(function Confirmation({
-  children,
-  className,
-  ...rest
-}: Omit<IBottomSheetProps, "dim" | "notch">) {
-  const width = useScreen(selectWidth);
-  const classes = useClassNames("confirmation-sheet", className, {
-    mobile: Devices.IS_MOBILE_BROWSER && width < 670,
-  });
-  return (
-    <BodyPortal>
-      <BottomSheet dim notch className={classes} {...rest}>
-        <Closer onClick={rest.close} aria-label="Close Dialog" />
-        {children}
-      </BottomSheet>
-    </BodyPortal>
-  );
-});
+export const Confirmation = memo(
+  forwardRef(function Confirmation(
+    { children, className, ...rest }: Omit<IBottomSheetProps, "dim" | "notch">,
+    ref: ForwardedRef<ISheetController>,
+  ) {
+    const width = useScreen(selectWidth);
+    const classes = useClassNames("confirmation-sheet", className, {
+      mobile: Devices.IS_MOBILE_BROWSER && width < 670,
+    });
+    return (
+      <BodyPortal>
+        <BottomSheet dim notch ref={ref} className={classes} {...rest}>
+          <Closer onClick={rest.close} aria-label="Close Dialog" />
+          {children}
+        </BottomSheet>
+      </BodyPortal>
+    );
+  }),
+);
