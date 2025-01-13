b97cd08405ba4e731906f0a4f4648318b695f87b
Alex Figliolia <alexfigliolia@gmail.com>
2025-01-13 12:59:17 -0500

    Tinker with property loading states

--- src/Pages/Properties/PropertyList/PropertyLink/Skeleton/index.tsx
+++ src/Pages/Properties/PropertyList/PropertyLink/Skeleton/index.tsx
@@ -1,32 +1,34 @@
 import { memo } from "react";
+import type { GradiumImage } from "GraphQL/Types";
 import type { Propless } from "Types/React";
 import { Slider } from "../Slider";
 import "../styles.scss";
 import "./styles.scss";
 
-export const PropertyLinkSkeleton = memo(function PropertyLinkSkeleton(
-  _: Propless,
-) {
-  return (
-    <div className="property-link skeleton">
-      <Slider images={[]} />
-      <div className="pl-info">
-        <h3 className="skeleton-text">Loading Property</h3>
-        <address>
-          <p className="skeleton-text">Loading Address</p>
-          <p className="skeleton-text">Unit</p>
-          <p>
-            <span className="skeleton-text">Loading City</span>
-            &nbsp;&nbsp;
-            <span className="skeleton-text">State</span>
-            &nbsp;&nbsp;
-            <span className="skeleton-text">and Zip Code</span>
-          </p>
-        </address>
-        <div>
-          <div className="link-placeholder skeleton-text" />
+const EMPTY: GradiumImage[] = [];
+
+export const PropertyLinkSkeleton = memo(
+  function PropertyLinkSkeleton(_: Propless) {
+    return (
+      <div className="property-link skeleton">
+        <Slider images={EMPTY} />
+        <div className="pl-info">
+          <h3 className="skeleton-text">Loading Property</h3>
+          <address>
+            <p className="skeleton-text">Loading Address</p>
+            <p className="skeleton-text">Unit</p>
+            <p>
+              <span className="skeleton-text">Loading City</span>
+              <span className="skeleton-text">State</span>
+              <span className="skeleton-text">and Zip</span>
+            </p>
+          </address>
+          <div>
+            <div className="link-placeholder skeleton-text" />
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+  () => true,
+);
